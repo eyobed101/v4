f418d49deee1bda13178513731a738c380c6029f
eyobed101 <eyocjjazz101@gmail.com>
2025-07-21 19:31:26 +0300

    just for commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-robots-txt`,
+    'gatsby-plugin-htaccess',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
