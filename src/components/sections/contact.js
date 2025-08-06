6962d182f0f29aec7b3a23c533390575b9d889ae
eyobed101 <eyocjjazz101@gmail.com>
2025-08-06 11:44:16 +0300

    open to work

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,8 +60,9 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+        I’m always open to new opportunities and connections! Feel free to reach out—whether you
+        have a question, a project in mind, or just want to say hello, I’ll do my best to respond
+        promptly.
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
