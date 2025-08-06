c77fc728e89dd61d9f6146e0c47416d96b18977f
eyobed101 <eyocjjazz101@gmail.com>
2025-08-06 11:40:54 +0300

    motion in layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,92 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import styled, { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider, keyframes } from 'styled-components';
 import { Head, Loader, Nav, Social, Email, Footer } from '@components';
 import { GlobalStyle, theme } from '@styles';
+
+// Starry background components
+const animStar = keyframes`
+  from {
+    transform: translateY(0px);
+  }
+  to {
+    transform: translateY(-2000px);
+  }
+`;
+
+const generateBoxShadows = n => {
+  let value = '';
+  for (let i = 0; i < n; i++) {
+    value += `${Math.floor(Math.random() * 2000)}px ${Math.floor(Math.random() * 2000)}px #FFF`;
+    if (i < n - 1) {
+      value += ', ';
+    }
+  }
+  return value;
+};
+
+const Stars = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+`;
+
+const Stars1 = styled.div`
+  width: 1px;
+  height: 1px;
+  background: transparent;
+  box-shadow: ${generateBoxShadows(700)};
+  animation: ${animStar} 50s linear infinite;
+
+  &:after {
+    content: ' ';
+    position: absolute;
+    top: 2000px;
+    width: 1px;
+    height: 1px;
+    background: transparent;
+    box-shadow: ${generateBoxShadows(700)};
+  }
+`;
+
+const Stars2 = styled.div`
+  width: 2px;
+  height: 2px;
+  background: transparent;
+  box-shadow: ${generateBoxShadows(200)};
+  animation: ${animStar} 100s linear infinite;
+
+  &:after {
+    content: ' ';
+    position: absolute;
+    top: 2000px;
+    width: 2px;
+    height: 2px;
+    background: transparent;
+    box-shadow: ${generateBoxShadows(200)};
+  }
+`;
+
+const Stars3 = styled.div`
+  width: 3px;
+  height: 3px;
+  background: transparent;
+  box-shadow: ${generateBoxShadows(100)};
+  animation: ${animStar} 150s linear infinite;
+
+  &:after {
+    content: ' ';
+    position: absolute;
+    top: 2000px;
+    width: 3px;
+    height: 3px;
+    background: transparent;
+    box-shadow: ${generateBoxShadows(100)};
+  }
+`;
 
 const StyledContent = styled.div`
   display: flex;
@@ -53,6 +137,11 @@ const Layout = ({ children, location }) => {
       <div id="root">
         <ThemeProvider theme={theme}>
           <GlobalStyle />
+          <Stars>
+            <Stars1 />
+            <Stars2 />
+            <Stars3 />
+          </Stars>
 
           <a className="skip-to-content" href="#content">
             Skip to Content
